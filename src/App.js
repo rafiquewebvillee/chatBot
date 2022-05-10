a9ee8b2f2fac9de127c5bb18083d93b97f5607ab
Rafique <rafique.webvillee@gmail.com>
2022-05-10 10:30:22 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import { useState } from 'react';
 import { Animated } from "react-animated-css";
 
 function App() {
-  const[chatBotShow, setChatBoatShow] = useState("")
+  const [chatBotShow, setChatBoatShow] = useState("")
+  const [animation, setAnimation] = useState("")
   const clickChat = () => {
     //console.log("Clicked")
     setChatBoatShow(!chatBotShow)
+    setAnimation(!animation)
   }
   return (
     <div className="App">
-      <div className='buttonChat' onClick={()=> clickChat() }>
-          <h1>Test</h1>
+      <div className='buttonChat' onClick={() => clickChat()}>
+        <h1>Test</h1>
       </div>
-      {/* <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={1000} animationOut="fadeOut" isVisible={true}> */}
-    {/* <div>
-        hello world ;)
-    </div> */}
-{/* </Animated> */}
-      
-      <ChatBoat chatBotShow={chatBotShow} setChatBoatShow={setChatBoatShow}/>
+      <ChatBoat animation={animation} setAnimation={setAnimation} chatBotShow={chatBotShow} setChatBoatShow={setChatBoatShow} />
     </div>
   );
 }
--- src/ChatBot/index.js
+++ src/ChatBot/index.js
@@ -10,38 +10,55 @@ function ChatBoat(props) {
       >
         <div className="bodyChat">
           <div className="headerChat">
-            <h2 className="assistance_name">IRIS</h2>
-            <h4 className="assistanceGreeting">
-              Hello <img src="../Assets/Images/hand.png" alt="svg" />
-            </h4>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={100} animationInDelay={100} animationOut="fadeOut" isVisible={true}>
+              <h2 className="assistance_name">IRIS</h2>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={200} animationInDelay={200} animationOut="fadeOut" isVisible={true}>
+              <h4 className="assistanceGreeting">
+                Hello <img className="ml-20" src="../Assets/Images/hand.png" alt="svg" />
+              </h4>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={300} animationInDelay={300} animationOut="fadeOut" isVisible={true}>
             <p className="intro_assistance">
               I am Iris, a Virtual Assistant
               <br />
               How may I help you?
             </p>
+            </Animated>
+            
           </div>
           <div div className="bodyChat_inner">
             <img
               className="assistanceIcon"
               src="../Assets/Images/assistance.png"
             />
-            <p className="classFreq">Frequently Asked Questions <img alt="question" src="../Assets/Images/questionMark.png" /></p>
-            <div className="inputFreq">
-              <input type="radio" className="inputRadio" name="freqQusetion"/>
-              <label>Can I redeem my FB before the original term?</label>
-            </div>
-            <div className="inputFreq">
-              <input type="radio" className="inputRadio" name="freqQusetion"/>
-              <label>How do I pay my Credit card bill?</label>
-            </div>
-            <div className="inputFreq">
-              <input type="radio" className="inputRadio" name="freqQusetion"/>
-              <label>How can I get my Account Statement?</label>
-            </div>
-            <div className="inputFreq">
-              <input type="radio" className="inputRadio" name="freqQusetion"/>
-              <label>What is the tenure of Fixed Deposit?</label>
-            </div>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn="" animationInDuration={400} animationInDelay={400} animationOut="fadeOut" isVisible={true}>
+             <p className="classFreq">Frequently Asked Questions <img alt="question" src="../Assets/Images/questionMark.png" /></p>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={500} animationInDelay={500} animationOut="fadeOut" isVisible={true}>
+              <div className="inputFreq">
+                <input type="radio" className="inputRadio" name="freqQusetion" />
+                <label>Can I redeem my FB before the original term?</label>
+              </div>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={600} animationInDelay={600} animationOut="fadeOut" isVisible={true}>
+              <div className="inputFreq">
+                <input type="radio" className="inputRadio" name="freqQusetion" />
+                <label>How do I pay my Credit card bill?</label>
+              </div>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={700} animationInDelay={700} animationOut="fadeOut" isVisible={true}>
+              <div className="inputFreq">
+                <input type="radio" className="inputRadio" name="freqQusetion" />
+                <label>How can I get my Account Statement?</label>
+              </div>
+            </Animated>
+            <Animated className={props.animation ? "fadeInUp" : ""}  animationIn=""  animationInDuration={800} animationInDelay={800} animationOut="fadeOut" isVisible={true}>
+              <div className="inputFreq">
+                <input type="radio" className="inputRadio" name="freqQusetion" />
+                <label>What is the tenure of Fixed Deposit?</label>
+              </div>
+            </Animated>
           </div>
         </div>
       </div>
